19b62966e8643bbf40dedc2fdf5bb735daf26408
Marc Balestreri <marcbal77@ymail.com>
2019-08-23 00:12:12 -0700

    Initiate weather api instance from OpenWeatherMap.org

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -8,8 +8,13 @@ import Form from "./components/Form";
 //Import Weather Component
 import Weather from "./components/Weather";
 
+const API_KEY = "3948607ceafbbf3a5a2fe6a01e37c67f"
+
 // initialize component
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = async await
+  }
   //apply render method to display data
   render() {
     return (
